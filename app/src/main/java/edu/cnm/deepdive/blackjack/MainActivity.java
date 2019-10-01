e2f55d1576f4c079060e3d376e91681615316ceb
Nick Bennett <nick@nickbenn.com>
2019-10-01 14:29:04 -0600

    Initial commit

---
+++ app/src/main/java/edu/cnm/deepdive/blackjack/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.blackjack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
